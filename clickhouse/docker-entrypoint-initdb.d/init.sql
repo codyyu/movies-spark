6203c780c900aa788232f8665381bca6eb18bb98
Cody Yu <codyyu717@gmail.com>
2023-10-18 21:27:38 -0700

    add more table schema

--- clickhouse/docker-entrypoint-initdb.d/init.sql
+++ clickhouse/docker-entrypoint-initdb.d/init.sql
@@ -48,3 +48,42 @@ ORDER BY tconst;
 
 INSERT INTO movies.title_crew
 FROM INFILE 'data/clean/title-crew.parquet' FORMAT Parquet;
+
+CREATE TABLE IF NOT EXISTS movies.title_principals (
+    tconst String,
+    ordering Int64,
+    nconst String,
+    category String,
+    job String,
+    characters String
+)
+ENGINE = MergeTree
+ORDER BY tconst;
+
+INSERT INTO movies.title_principals
+FROM INFILE 'data/clean/title-principals.parquet' FORMAT Parquet;
+
+CREATE TABLE IF NOT EXISTS movies.title_ratings (
+    tconst String,
+    averageRating Float64,
+    numVotes Int64
+)
+ENGINE = MergeTree
+ORDER BY tconst;
+
+INSERT INTO movies.title_ratings
+FROM INFILE 'data/clean/title-ratings.parquet' FORMAT Parquet;
+
+CREATE TABLE IF NOT EXISTS movies.name_basics (
+    nconst String,
+    primaryName String,
+    birthYear String,
+    deathYear String,
+    primaryProfession String,
+    knownForTitles String
+)
+ENGINE = MergeTree
+ORDER BY nconst;
+
+INSERT INTO movies.name_basics
+FROM INFILE 'data/clean/name-basics.parquet' FORMAT Parquet;
